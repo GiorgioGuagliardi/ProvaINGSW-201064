cd9154a72fe54cd919c44fcc94cfa8beb0163bdf
Giorgio <gegio.guagliardi@gmail.com>
2020-11-04 11:06:21 +0100

    Primo commit

---
+++ ProvaINGSW201064/src/main/java/INGSW2020/ProvaINGSW201064/App.java
@@ -0,0 +1,13 @@
+package INGSW2020.ProvaINGSW201064;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ ProvaINGSW201064/src/test/java/INGSW2020/ProvaINGSW201064/AppTest.java
@@ -0,0 +1,20 @@
+package INGSW2020.ProvaINGSW201064;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
